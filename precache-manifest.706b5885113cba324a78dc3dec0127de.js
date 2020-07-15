617aa47cb01e9e91b8040c91dfa09ce0103f3853
tavadmin <tavadmin@codi-priyanka.internal.tavisca.com>
2020-07-15 12:34:44 +0530

    Updates

---
+++ precache-manifest.706b5885113cba324a78dc3dec0127de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679498d9bfe2acc2b111340e98bf2395",
+    "url": "/%7Bdemo%7D/index.html"
+  },
+  {
+    "revision": "a891e53ff2c4496b100e",
+    "url": "/%7Bdemo%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ac0184666dc72fb79a4",
+    "url": "/%7Bdemo%7D/static/js/2.4250e7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Bdemo%7D/static/js/2.4250e7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a891e53ff2c4496b100e",
+    "url": "/%7Bdemo%7D/static/js/main.baf38eb0.chunk.js"
+  },
+  {
+    "revision": "4006f0f7b2ee3c66b077",
+    "url": "/%7Bdemo%7D/static/js/runtime-main.179af097.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7Bdemo%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
